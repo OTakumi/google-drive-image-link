9c12b316113d0dceda37f8f4cfc1e96fb928aaf1
OTakumi <takumi.o.0410@gmail.com>
2022-12-17 21:52:41 +0900

    A function to generate URLs from command line arguments was implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let base_url = &args[1];
+
+    // Combine URL and id
+    let rslt = "https://drive.google.com/uc?export=view&id=".to_string() + &base_url[32..65];
+
+    println!("id: {:?}", rslt);
 }
